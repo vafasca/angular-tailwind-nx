85d0ba94f6ee771bcb3ef12e732d759e5338a74e
Bruno Alejandro Ortiz Velasquez <counter_19_95@hotmail.com>
2023-02-02 10:59:35 -0400

    last changes

--- apps/app1/src/app/app.module.ts
+++ apps/app1/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//
+import { Lib1Module } from '@angular-tailwind-nx/lib1';
+//adding new lib2 import
+import { Lib2Module } from '@angular-tailwind-nx/lib2';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, Lib1Module, Lib2Module],
   providers: [],
   bootstrap: [AppComponent],
 })
